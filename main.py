28b90cf537a8fb28b2273adcb81a0eb023660faa
Gwardii <62251613+Gwardii@users.noreply.github.com>
2020-04-01 12:58:45 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -6,3 +6,15 @@ app = FastAPI()
 @app.get("/")
 async def read_main():
     return {"message": "Hello World during the coronavirus pandemic!"}
+@app.get("/method/")
+async def get():
+    return {"message": "GET"}
+@app.post("/method/")
+async def get():
+    return {"message": "POST"}
+@app.put("/method/")
+async def get():
+    return {"message": "PUT"}
+@app.delete("/method/")
+async def get():
+    return {"message": "DELETE"}
