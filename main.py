abb748acf1d407afaa93b600ea77f341920d64b1
Gwardii <62251613+Gwardii@users.noreply.github.com>
2020-04-01 13:07:42 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ async def read_main():
     return {"message": "Hello World during the coronavirus pandemic!"}
 @app.get("/method/")
 async def get():
-    return {"message": "GET"}
+    return {"method": "GET"}
 @app.post("/method/")
 async def get():
-    return {"message": "POST"}
+    return {"method": "POST"}
 @app.put("/method/")
 async def get():
-    return {"message": "PUT"}
+    return {"method": "PUT"}
 @app.delete("/method/")
 async def get():
-    return {"message": "DELETE"}
+    return {"method": "DELETE"}
