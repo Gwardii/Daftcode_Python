b7ea67d6431350635b2b43b5e3eb423555b482b8
Gwardii <62251613+Gwardii@users.noreply.github.com>
2020-04-01 13:15:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ async def read_main():
     return {"message": "Hello World during the coronavirus pandemic!"}
 @app.get("/method/")
 async def get():
-    return {"method": "GET"}
+    return {"method": "METHOD"}
 @app.post("/method/")
 async def get():
-    return {"method": "POST"}
+    return {"method": "METHOD"}
 @app.put("/method/")
 async def get():
-    return {"method": "PUT"}
+    return {"method": "METHOD"}
 @app.delete("/method/")
 async def get():
-    return {"method": "DELETE"}
+    return {"method": "METHOD"}
