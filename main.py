912339a9ba791086259a04ae5140c1f3878e2919
Gwardii <62251613+Gwardii@users.noreply.github.com>
2020-04-26 23:56:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,59 @@
-from starlette.responses import RedirectResponse
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
-from fastapi import FastAPI, Response, status
-from fastapi import Depends, Cookie, HTTPException
-from hashlib import sha256
-
-import secrets
-
-
+from fastapi import FastAPI, HTTPException, Response, Request, Cookie
+from fastapi.responses import RedirectResponse
+from pydantic import BaseModel
+class Item(BaseModel):
+    name: str
+    surename: str
 app = FastAPI()
-
-
-@app.get("/welcome")
-def get_welcome():
-	return "Hello!"
-
-
-app.secret_key = "very constatn and random secret, best 64 characters"
-app.tokens_list = []
-security = HTTPBasic()
-
-
-def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
-    correct_username = secrets.compare_digest(credentials.username, "trudnY")
-    correct_password = secrets.compare_digest(credentials.password, "PaC13Nt")
-    print(f"{credentials.username}")
-    print(f"{credentials.password}")
-    if not (correct_username and correct_password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            # detail="Incorrect email or password",
-            # headers={"WWW-Authenticate": "Basic"},
-        )
-    return credentials.username
-
-
-
-@app.post("/login")
-def login(
-    user: str, password: str, response: Response,
-    credentials_user = Depends(get_current_username)
-    ):
-    
-    session_token = sha256(bytes(f"{user}{password}{app.secret_key}", encoding='utf8')).hexdigest()
-    app.tokens_list.append(session_token)
-    
-    response.set_cookie(key="session_token", value=session_token)
-    
-        
-    response = RedirectResponse(url = "/welcome")
-    response.status_code = status.HTTP_302_FOUND
-    
+N=0
+Patients=[]
+@app.get("/")
+async def read_main():
+    return {"message": "Hello World during the coronavirus pandemic!"}
+@app.get("/wel/")
+async def get():
+    return {"message": Patients}
+@app.post("/method/")
+async def get():
+    return {"method": "POST"}
+@app.put("/method/")
+async def get():
+    return {"method": "PUT"}
+@app.delete("/method/")
+async def get():
+    return {"method": "DELETE"}
+@app.post("/patient/")
+async def fun(item: Item):
+    global Patients
+    global N
+    N+=1
+    Patients += [item]
+    return {"id": N, "patient": item}
+@app.get("/patient/{pk}")
+async def fun(pk: int):
+    global Patients
+    if pk > len(Patients):
+        raise HTTPException(status_code=204)
+    patient=Patients[pk-1]
+    return patient
+@app.post("/cookie-and-object/")
+def create_cookie(response: Response):
+    response.set_cookie(key="session", value="fake-cookie-session-value")
+    return {"message": "Come to the dark side, we have cookies"}
+@app.post("/login/")
+def create_cookie(request: Request, response: Response):
+    global Patients
+    Patients.append(request.headers['Authorization'])
+    response=RedirectResponse(url='/welcome/')
+    if(request.headers['Authorization']=="Basic dHJ1ZG5ZOlBhQzEzTnQ="):
+        response=RedirectResponse(url='/welcome/')
+        response.set_cookie(key="session", value="abcd")
+        return response
+    return {"message": "Wrong login"}
     return response
+@app.post("/welcome/")
+def create_cookie(*, response: Response, session: str = Cookie(None)):
+    if (session != "abcd"):
+        raise HTTPException(status_code=401, detail="Unathorised")
+    response.set_cookie(key="session", value=session)
+    return {"message": "wow"}
