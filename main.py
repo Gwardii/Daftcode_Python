3d9c4a267dac5a50bdbafba960cbafa9bf075600
Gwardii <62251613+Gwardii@users.noreply.github.com>
2020-04-27 00:52:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -52,5 +52,4 @@ def create_cookie(request: Request, response: Response):
 def create_cookie(*, response: Response, session: str = Cookie(None)):
     if (session != "abcd"):
         raise HTTPException(status_code=401, detail="Unathorised")
-    response.set_cookie(key="session", value=session)
     return {"message": "wow"}
