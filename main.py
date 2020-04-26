ba451390e0951600439ae64a328f67630179f0fd
Gwardii <62251613+Gwardii@users.noreply.github.com>
2020-04-26 16:59:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ async def fun(pk: int):
         raise HTTPException(status_code=204)
     patient=Patients[pk-1]
     return patient
-@app.post("/login/")
+'''@app.post("/login/")
 def create_cookie(login: str, pass: str, response: Response):
     if(login==trudnY and pass==PaC13Nt):
         session_token = sha256(bytes(f"{login}{pass}{app.secret_key}")).hexdigest()
