cb8ab0d683e5c3cd0b2c5969114a9bb6976e4098
Gwardii <62251613+Gwardii@users.noreply.github.com>
2020-04-26 18:58:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -47,12 +47,12 @@ def create_cookie(response: Response):
 @app.post("/login/")
 def create_cookie(login: str, password: str, response: Response):
     response.set_cookie(key="fake_session", value="fake-cookie-session-value")
-    if(login=='trudnY' and password=='PaC13Nt'):
+    if(login=='a' and password=='a'):
         
         return RedirectResponse(url='/welcome/')
     return {"message": "Wrong login"}
 @app.post("/welcome/")
 def create_cookie(*, response: Response, fake_session: str = "nic"):
-    if (fake_session != "fake-cookie-session-value"):
+    if (fake_session == "fake-cookie-session-value"):
         raise HTTPException(status_code=403, detail="Unathorised")
     response.set_cookie(key="session_token", value=session_token)
