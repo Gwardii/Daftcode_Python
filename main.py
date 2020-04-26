c87f9eeeb049c6874f89c58f88f7d8a5a52ff745
Gwardii <62251613+Gwardii@users.noreply.github.com>
2020-04-27 00:11:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -42,8 +42,6 @@ def create_cookie(response: Response):
     return {"message": "Come to the dark side, we have cookies"}
 @app.post("/login/")
 def create_cookie(request: Request, response: Response):
-    global Patients
-    Patients.append(request.headers['Authorization'])
     response=RedirectResponse(url='/welcome/')
     if(true):
         response.set_cookie(key="session", value="abcd")
