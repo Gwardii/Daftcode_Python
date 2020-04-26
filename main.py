6977235be38f07edc2351d8869f60b7d883fa122
Gwardii <62251613+Gwardii@users.noreply.github.com>
2020-04-26 17:05:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
-
-
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-
-from hashlib import sha256
-from fastapi import FastAPI, Response, Cookie, HTTPException
 
 class Item(BaseModel):
     name: str
     surename: str
 
 
-
 app = FastAPI()
 
 N=0
 Patients=[]
-Database=[]
 @app.get("/")
 async def read_main():
     return {"message": "Hello World during the coronavirus pandemic!"}
@@ -45,17 +39,3 @@ async def fun(pk: int):
         raise HTTPException(status_code=204)
     patient=Patients[pk-1]
     return patient
-'''@app.post("/login/")
-def create_cookie(login: str, pass: str, response: Response):
-    if(login==trudnY and pass==PaC13Nt):
-        session_token = sha256(bytes(f"{login}{pass}{app.secret_key}")).hexdigest()
-        global Database.append(session_token)
-        response.set_cookie(key="session_token", value=session_token)
-        return RedirectResponse(url='\welcome')
-    return {"message": "Wrong login"}
-
-@app.get("/welcome/")
-def create_cookie(*, response: Response, session_token: str = Cookie(None)):
-    if session_token not in Database......... :
-        raise HTTPException(status_code=403, detail="Unathorised")
-    response.set_cookie(key="session_token", value=session_token)
