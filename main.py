be0f4d0461b586607805e64be5dd96855d09593a
Gwardii <62251613+Gwardii@users.noreply.github.com>
2020-04-27 01:36:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ security = HTTPBasic()
 app.secret_key = "very constatn and random secret, best 64 characters, here it is."
 
 session_tokens = []
-@app.get("/"):
+@app.get("/")
 def funkcja():
     return {"message": "Witaj!"}
-@app.get("/welcome/"):
+@app.get("/welcome/")
 def funkcja():
     return {"message": "Witaj!"}
 @app.post("/login")
@@ -23,7 +23,8 @@ def get_current_user(response: Response, credentials: HTTPBasicCredentials = Dep
     if not (correct_username and correct_password):
         raise HTTPException(status_code=401, detail="Incorrect email or password")
     session_token = sha256(bytes(f"{credentials.username}{credentials.password}{app.secret_key}", encoding='utf8')).hexdigest()
-    response = RedirectResponse(url='/welcome/')	
+    response = RedirectResponse(url='/welcome/')
+    response.status_code = status.HTTP_302_FOUND
     response.set_cookie(key="session_token", value=session_token)
     session_tokens.append(session_token)
     return response
